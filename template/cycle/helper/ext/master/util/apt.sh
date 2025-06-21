56e8c74474a89dee62600cb74342dd485d8a29c8
samwhelp <samwhelp@users.noreply.github.com>
2025-06-22 01:51:13 +0800

    fix

--- template/cycle/helper/ext/master/util/apt.sh
+++ template/cycle/helper/ext/master/util/apt.sh
@@ -10,10 +10,10 @@ util_apt_source_list_create_simple () {
 	local build_suite="${2}"
 
 cat << __EOF__
-deb ${package_repo_url} ${build_suite} main non-free-firmware
-deb ${package_repo_url} ${build_suite}-updates main non-free-firmware
-deb ${package_repo_url} ${build_suite}-backports main non-free-firmware
-deb http://security.debian.org/debian-security/ ${build_suite}-security main non-free-firmware
+deb ${package_repo_url} ${build_suite} main contrib non-free non-free-firmware
+deb ${package_repo_url} ${build_suite}-updates main contrib non-free non-free-firmware
+deb ${package_repo_url} ${build_suite}-backports main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security/ ${build_suite}-security main contrib non-free non-free-firmware
 __EOF__
 
 }
